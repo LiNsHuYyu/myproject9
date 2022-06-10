1a71f4e1065c4dc8d55f9674c79e739c735d050f
LiNsHuYyu <mich030403@gmail.com>
2022-06-10 14:49:56 +0800

    Hello world with class Person

---
+++ src/main/java/fju/edu/Person.java
@@ -0,0 +1,9 @@
+package fju.edu;
+
+public class Person {
+    public void greeting(){
+        //System.out.println("Hello World!");
+        Person person = new Person();
+        person.greeting();
+    }
+}
